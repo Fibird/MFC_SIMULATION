6e6ba7371909ecdf13b052c3ffea00bd1241f0ea
Chaoyang Liu <chaoyanglius@gmail.com>
2016-06-03 17:43:35 +0800

    First commit

---
+++ Frame1/My.cpp
@@ -0,0 +1,13 @@
+#include "My.h"
+
+CMyWinApp theApp;	//global object
+
+//--------------------------------------------------
+//main function
+//--------------------------------------------------
+
+int main()
+{
+	CWinApp *pApp = AfxGetApp();
+	return 0;
+}
